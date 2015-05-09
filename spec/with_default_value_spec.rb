d4788b743ff8db2870430feddc3e1dfe8d80cee8
Shu Fujita <osorubeki.fujita@gmail.com>
2015-05-09 13:16:27 +0900

    20150509_131624 Add test

--- spec/with_default_value_spec.rb
+++ spec/with_default_value_spec.rb
@@ -9,4 +9,17 @@ describe WithDefaultValue do
     expect( ::WithDefaultValue::VERSION ).to eq( version )
     expect( ::Deplo.version_check( ::WithDefaultValue::VERSION , spec_filename ) ).to eq( true )
   end
+  
+  describe BasicObjectExt do
+    describe "\#with_default_value" do
+      it "checkes instance by \'blank?\' (defined by ActiveSupport, not \'nil?\'). If the instance is evaluated as blank, returns default value." do
+        expect( nil.with_default_value( "abc" ) ).to eq( "abc" )
+        expect( "def".with_default_value( "abc" ) ).to eq( "def" )
+        expect( " ".with_default_value( "abc" ) ).to eq( "abc" )
+        expect( 2.with_default_value( "abc" ) ).to eq(2)
+        expect( nil.with_default_value(3) ).to eq(3)
+      end
+    end
+  end
+
 end
